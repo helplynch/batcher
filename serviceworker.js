77ce6deeacb2cb84478724d0a6b608493c34c383
helplynch <108618400+helplynch@users.noreply.github.com>
2023-07-04 21:52:27 -0400

    Service Worker
    Full release - Dropped the developer tokens and added public

--- serviceworker.js
+++ serviceworker.js
@@ -6,21 +6,7 @@ const hashVer = "5"
 //    ( s | h | 4 | d | y | y ) was here <3
 //     \_/ \_/ \_/ \_/ \_/ \_/ 
 
-const devtokens = process.env.devkeys
+const devtokens = []
 
 
-if (devtokens.includes(token)) {
-  developerMode = true
-} else {
-  developerMode = false
-}
-
-setInterval(function() {
-if (developerMode == true) {
-  if (devtokens.includes(token)) {
-  developerMode = true
-  } else {
-  developerMode = false
-  }
-}
-}, 200);
+developerMode = true
