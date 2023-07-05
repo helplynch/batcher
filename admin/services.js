6a23e63aa3eac80a7463c96722b2807ab87b26ce
helplynch <108618400+helplynch@users.noreply.github.com>
2023-07-05 02:34:10 -0400

    Update services.js

---
+++ admin/services.js
@@ -0,0 +1,24 @@
+const adminPageComplete = false
+
+// Without correct permissions, you will not be able to access the admin panel.
+
+function checkPerms() {
+  if (window.location.href == "https://www.batcher.xyz/admin") {
+    let [code, response] = await request('info', 'PUT', {
+      token: token
+    });
+    if(code == 200) {
+      response = JSON.parse(response);
+      username = response.name;
+      verified = response.verified;
+    }
+  if (response.verified == "admin") {
+    console.log("User is allowed to enter page.");
+  } else {
+    console.log("User tried to enter /admin without permission.");
+    window.location.href = "https://www.batcher.xyz";
+  }
+  }
+}
+
+checkPerms();
