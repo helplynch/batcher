b1620eeecd27d166c8293732dba790eecbcd15c3
helplynch <108618400+helplynch@users.noreply.github.com>
2023-07-05 02:36:24 -0400

    Update services.js

--- admin/services.js
+++ admin/services.js
@@ -2,6 +2,25 @@ const adminPageComplete = false
 
 // Without correct permissions, you will not be able to access the admin panel.
 
+const serverURL = 'https://batcherbackend.codingmadnessyt.repl.co/'
+async function request(url, method, body) {
+  let data = {
+    method: method,
+        headers: {
+            "cache": "no-cache",
+            "Content-Type": "application/json"
+        }
+  };
+  if(body){
+        if (typeof body == "object" && body instanceof FormData == false) {
+            body = JSON.stringify(body);
+        }
+    data.body = body;
+  }
+    let res = await fetch(serverURL + url, data);
+  return [res.status, await res.text()];
+}
+
 async function checkPerms() {
   if (window.location.href == "https://www.batcher.xyz/admin") {
     let [code, response] = await request('info', 'PUT', {
